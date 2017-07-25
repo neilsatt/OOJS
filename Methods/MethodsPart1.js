f3f4fa18c558c645a25d7472bcc801e6b6d67df8
neilsatt <neilsatt@gmail.com>
2017-07-24 20:56:29 -0600

    initial commit

---
+++ Methods/MethodsPart1.js
@@ -0,0 +1,30 @@
+/*
+    An Object is a container for:
+        Values in the form of properties and 
+        Functionality in the form of methods
+        They have state and behavior 
+        Methods may return values
+        Objects provide data storage in properties
+        The property name is a key
+        The contents of a property is a value
+    
+    Native Objects in JavaScript:
+      Number, String, Array, Boolean, Object
+      
+    Host Objects (environment like browser)
+        document
+        console
+        Element 
+        
+    The Objects you create     
+    
+    Example Object:
+    Radio
+    state = on/off
+    
+    
+*/
+
+ // document is the object, getElement is the method
+var h1 = document.getElementById("header"); 
+h1.innerHTML = "JavaScript"; // innerHTML = content
